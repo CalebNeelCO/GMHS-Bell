cfd44ee0a26d60ac9d0ffc1cf93acf049b950823
Caleb Neel <34864122+CalebNeelCO@users.noreply.github.com>
2018-03-01 21:48:52 -0700

    Update to new sumMod

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
-def gettime_ntp(addr='time.nist.gov'):
-    # http://code.activestate.com/recipes/117211-simple-very-sntp-client/
-    import socket
-    import struct
-    import sys
-    import time
-    TIME1970 = 2208988800L      # Thanks to F.Lundh
-    client = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
-    data = '\x1b' + 47 * '\0'
-    client.sendto( data, (addr, 123))
-    data, address = client.recvfrom( 1024 )
-    if data:
-        t = struct.unpack( '!12I', data )[10]
-        t -= TIME1970
-        return time.ctime(t),t
+#from 9f85ab77e1a11d0eebdb import *
+from 9f85ab77e1a11d0eebdb import *
+#print(gettime_ntp())
