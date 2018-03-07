3758bad0110dc5dee948070fada64494070f5bb6
Caleb Neel <34864122+CalebNeelCO@users.noreply.github.com>
2018-03-07 11:29:35 -0700

    Update main.py

--- main.py
+++ main.py
@@ -14,10 +14,6 @@ dict1 = {
 4: {9:"20", 10:"45", 11:"25", 12:"45", 2:"10", 3:"00"},
 5: {8:"50", 9:"45", 10:"45", 11:"40", 12:"20", 1:"10", 2:"05", 3:"00"}}
 
-# I WAS ABLE TO GET THE LIGHT TO FLASH FOR THREE SECONDS AND TURN OFF
-# BY IMPORTING TIME IN THE ACTUAL FUNCTION BUT I COULDN'T FIGURE OUT THE BUZZER
-
-
 c = True
 while c:
     time = datetime.datetime.now()
