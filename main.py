f528b0b8191a390a820bb340016cc3f45ffbab49
Caleb Neel <34864122+CalebNeelCO@users.noreply.github.com>
2018-03-08 13:09:09 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(18,GPIO.OUT)
-time.sleep(2)
 GPIO.output(18,GPIO.LOW)
+
+T = 5 #Time Before Bell
 
 dict1 = {
 1: {8:"50", 9:"45", 10:"45", 11:"40", 12:"20", 1:"10", 2:"05", 3:"00"},
@@ -14,8 +15,8 @@ dict1 = {
 4: {9:"20", 10:"45", 11:"25", 12:"45", 2:"10", 3:"00"},
 5: {8:"50", 9:"45", 10:"45", 11:"40", 12:"20", 1:"10", 2:"05", 3:"00"}}
 
-c = True
-while c:
+while True:
+    GPIO.output(18,GPIO.HIGH)
     time = datetime.datetime.now()
     day = time.weekday() + 1
     hour = time.hour
@@ -23,8 +24,7 @@ while c:
     import time
     for i in dict1[day]:
         if(hour == i):
-            if(mini == int(dict1[day][i])):
-                print("test")
+            if(mini == int(dict1[day][i])-T):
                 GPIO.output(18,GPIO.HIGH)
                 time.sleep(3)
                 GPIO.output(18,GPIO.LOW)
